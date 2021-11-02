004571c3c5cc4f4afa989ea738fbe142aeb7a119
azkaBashir45 <azka.ngxoft@gmail.com>
2021-11-02 17:46:27 +0500

    logout

--- controller/registerController.js
+++ controller/registerController.js
@@ -1,5 +1,8 @@
 const Register=require("./../model/userRegesteration");
+const Otp=require("./../model/otp")
 const crypto=require("crypto")
+
+const session=require('express-session')
 
 const key="Password"
 const algo="aes256"
@@ -40,7 +43,7 @@ const Post_Register= async(req,res)=>{
 
 
 //login
-const loginData=(req,res)=>{
+const loginData= async (req,res)=>{
     try
     {
         const loginResult= await Register.findOne({
@@ -57,7 +60,115 @@ catch(error){
 
 }
 }
+
+
+//Reset Password......
+//email send
+const emailSend=async (req,res)=>{
+ //send email before validate email
+ console.log(req.body.email)
+ const data= await Register.findOne({email:req.body.email})
+// console.log(data)
+ //response
+ const responseType={}
+
+if(data)
+{
+    const OtpCode=Math.floor((Math.random()*10000)+1);
+    const otpData=new Otp({
+        email:req.body.email,
+        code:OtpCode,
+        expiresIn:new Date().getTime()+300*1000
+    })
+    let otpResponse=await otpData.save();
+    responseType.statusText='Successs'
+    // mailer()
+    responseType.message='please check your email id'
+
+}
+else{
+    responseType.statusText='Error'
+    responseType.message='Email i is not exist'
+
+}
+
+
+   res.status(200).json(responseType)
+}
+
+
+//reset Pssword
+const changePassword=async (req,res)=>{
+    let data=await Otp.find({email:req.body.email,code:req.body.code});
+    const responseType={}
+    if(data){
+        let currentTime=new Date().getTime();
+        let diff=data.expiresIn-currentTime;
+        if(diff<0)
+        {
+            responseType.message='Token Expire',
+            responseType.statusText='error'
+        }
+        else
+        {
+            let user=await Register.findOne({email:req.body.email}) 
+            user.password=req.body.password;
+            user.save()
+            responseType.message='change Password Successfully',
+            responseType.statusText='success'
+        }
+    }
+    else
+    {
+        responseType.message='Invalid',
+        responseType.statusText='error'
+    }
+    res.status(200).json(responseType)
+ }
+
+//mailer
+const mailer=(email,otp)=>{
+    var nodemailer=require("nodemailer");
+    var transporter=nodemailer.createTransport({
+        service:"gmail",
+        port:587,
+        secure:false,
+        auth:{
+            user:'azka.ngxfot@gmail.com',
+            pass:"Azka1279945"
+        }
+    });
+    var mailOptions={
+        from:"azka.ngxfot@gmail.com",
+        subject:"sending",
+        text:"thank you sir"
+    };
+    transporter.sendMail(mailOptions,(error,info)=>{
+        if(error)
+        {
+            console.log('errror',error)
+        }
+        else{
+            console.log('email sent',+info.response);
+        }
+    })
+
+
+}
+
+
+const logOut=(req,res)=>{
+    req.logout();
+    res.status(200).json({
+     status: 'Bye!'
+    });
+}
+
+
 module.exports={
     Post_Register,
-    loginData
+    loginData,
+    emailSend,
+    changePassword,
+    logOut
 }
--- rotes/registerRouter.js
+++ rotes/registerRouter.js
@@ -1,6 +1,12 @@
 const registerController=require("../controller/registerController")
 const router=require("express").Router();
 
-router.post("/",registerController.Post_Register);
-router.post("/",registerController.loginData);
+router.post("/register",registerController.Post_Register);
+router.post("/login",registerController.loginData);
+
+//for reset password
+router.post("/emailSend",registerController.emailSend);
+router.post("/changePassword",registerController.changePassword);
+router.get("/logOut",registerController.logOut);
+
 module.exports=router;
